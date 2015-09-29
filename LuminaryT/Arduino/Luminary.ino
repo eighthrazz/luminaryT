e3baedbac13303e7a987e5e4272fc89015514a40
eighthrazz@gmail.com <eighthrazz@gmail.com>
2015-09-28 22:42:47 -0500

    initial commit

---
+++ LuminaryT/Arduino/Luminary.ino
@@ -0,0 +1,201 @@
+﻿/* Ports and Pins
+Direct port access is much faster than digitalWrite.
+You must match the correct port and pin as shown in the table below.
+
+Arduino Pin        Port        Pin
+13 (SCK)           PORTB       5
+12 (MISO)          PORTB       4
+11 (MOSI)          PORTB       3
+10 (SS)            PORTB       2
+9                  PORTB       1
+8                  PORTB       0
+7                  PORTD       7
+6                  PORTD       6
+5                  PORTD       5
+4                  PORTD       4
+3                  PORTD       3
+2                  PORTD       2
+1 (TX)             PORTD       1
+0 (RX)             PORTD       0
+A5 (Analog)        PORTC       5
+A4 (Analog)        PORTC       4
+A3 (Analog)        PORTC       3
+A2 (Analog)        PORTC       2
+A1 (Analog)        PORTC       1
+A0 (Analog)        PORTC       0
+*/
+
+// Defines for use with Arduino functions
+#define clockpin   13 // CL
+#define enablepin  10 // BL
+#define latchpin    9 // XL
+#define datapin    11 // SI
+
+// Defines for direct port access
+#define CLKPORT PORTB
+#define ENAPORT PORTB
+#define LATPORT PORTB
+#define DATPORT PORTB
+#define CLKPIN  5
+#define ENAPIN  2
+#define LATPIN  1
+#define DATPIN  3
+
+//
+const byte START_FRAME_INDICATOR = 0;
+const byte END_FRAME_INDICATOR = 255;
+
+// Variables for communication
+unsigned long SB_CommandPacket;
+int SB_CommandMode;
+int SB_BlueCommand;
+int SB_RedCommand;
+int SB_GreenCommand;
+
+// Define number of ShiftBrite modules
+#define NumLEDs 100
+
+// Create LED value storage array
+uint16_t LEDChannels[NumLEDs][3] = {0};
+
+byte inByte = 0;
+byte group = 0;
+
+// RGB Color
+int red = 0;
+int green = 0;
+int blue = 0;
+
+//
+unsigned long startTime = millis();
+unsigned long currentTime = 0;
+byte frames = 0;
+
+//
+void setup() {
+// Set pins to outputs and initial states
+pinMode(datapin, OUTPUT);
+pinMode(latchpin, OUTPUT);
+pinMode(enablepin, OUTPUT);
+pinMode(clockpin, OUTPUT);
+digitalWrite(latchpin, LOW);
+digitalWrite(enablepin, LOW);
+SPCR = (1<<SPE)|(1<<MSTR)|(0<<SPR1)|(0<<SPR0);
+
+  // Initialize the Serial Port
+Serial.begin(115200); //57600 or 115200
+
+// Reset the LEDs
+WriteLEDArray();
+}
+
+//
+void loop() {
+//
+inByte = Serial.read();
+
+//
+if(Serial.available() > 0) {
+//
+if(inByte == END_FRAME_INDICATOR) {
+//
+WriteLEDArray();
+
+//
+frames++;
+}
+
+//
+else if(inByte == START_FRAME_INDICATOR) {
+group = NumLEDs - 1;
+red = 0;
+green = 0;
+blue = 0;
+}
+
+//
+else if(red == 0) {
+red = inByte << 2;  
+      }
+      
+      //
+      else if(green == 0) {
+        green = inByte << 2;
+      }
+      
+      //
+      else if(blue == 0) {
+        blue = inByte << 2;
+               
+        // LEDChannels[LED GROUP][RGB 0-2] = Brightness 0-1024
+        LEDChannels[group][0] = red;   // RED
+        LEDChannels[group][1] = green; // GREEN
+        LEDChannels[group][2] = blue;  // BLUE
+        
+        //
+        group--;
+        red = 0;
+        green = 0;
+        blue = 0;
+      }
+      
+      Serial.write(group);
+        Serial.write(red);
+        Serial.write(green);
+        Serial.write(blue);
+    }
+}
+
+// Send all array values to chain
+void WriteLEDArray() {
+  // Write to PWM control registers
+  SB_CommandMode = B00; 
+
+  //
+  for (int i = 0; i<NumLEDs; i=""
+    SB_RedCommand = "LEDChannels"[i=""][0=""];   // RED
+SB_GreenCommand = LEDChannels[i][1]; // GREEN
+SB_BlueCommand = LEDChannels[i][2];  // BLUE
+SB_SendPacket();
+}
+
+//
+SB_Latch();
+
+// Write to current control registers
+SB_CommandMode = B01;
+
+//
+for (int z = 0; z < NumLEDs; z++) {
+    SB_SendPacket();   
+  }
+
+  //
+  SB_Latch();
+}
+
+// Load values into SPI register
+void SB_SendPacket() {
+  if (SB_CommandMode == B01) {
+    SB_RedCommand = 127;
+    SB_GreenCommand = 127;
+    SB_BlueCommand = 127;
+  }
+
+  SPDR = SB_CommandMode << 6 | SB_BlueCommand >> 4;
+  while(!(SPSR & (1<<SPIF
+  SPDR = "SB_BlueCommand" << 4 | SB_RedCommand >> 6;
+  while(!(SPSR & (1<<SPIF
+  SPDR = "SB_RedCommand" << 2 | SB_GreenCommand >> 8;
+  while(!(SPSR & (1<<SPIF
+  SPDR = "SB_GreenCommand"
+  while=""(!(SPSR="" & (1<<SPIF)));
+}
+
+// Latch values into PWM registers
+void SB_Latch() {
+delayMicroseconds(1);
+LATPORT += (1 << LATPIN);
+  LATPORT &= ~(1 << LATPIN);
+}
+    
---
+++ LuminaryT/ControlHub.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Diagnostics;
+
+namespace Luminary
+{
+    public class ControlHub : Hub
+    {
+        // send RGB data over network using ARTNET protocol
+        private static ArtNet.Engine ArtNetEngine;
+
+        // keep track of how long our ARTNET calls take 
+        private Stopwatch ArtNetStopWatch;
+
+        public ControlHub()
+        {
+            ArtNetEngine = null;
+            ArtNetStopWatch = new Stopwatch();
+        }
+
+        // SignalR client callback
+        // turns on/off power to LEDs and train
+        public void SetPower(Boolean on)
+        {
+            if (on)
+            {
+                // set pin mode to output 
+                String SetPinModeToOutputUrl = ArduinoHelper.Property.GetModeOutputUrl(ArduinoHelper.Property.PowerPin);
+                var content = ArduinoHelper.SendToArduino(SetPinModeToOutputUrl);
+                Clients.All.debug(String.Format("Power: url={0}, content={1}", SetPinModeToOutputUrl, content));
+
+                // set pin high
+                String SetPinHighUrl = ArduinoHelper.Property.GetDigitalWriteUrl(ArduinoHelper.Property.PowerPin, ArduinoHelper.Property.High);
+                content = ArduinoHelper.SendToArduino(SetPinHighUrl);
+                Clients.All.debug(String.Format("Power: url={0}, content={1}", SetPinHighUrl, content));
+            }
+            else
+            {
+                // set pin low
+                String SetPinLowUrl = ArduinoHelper.Property.GetDigitalWriteUrl(ArduinoHelper.Property.PowerPin, ArduinoHelper.Property.Low);
+                var content = ArduinoHelper.SendToArduino(SetPinLowUrl);
+                Clients.All.debug(String.Format("Power: url={0}, content={1}", SetPinLowUrl, content));
+            }
+
+            // cache power value
+            String SetPowerValueUrl = ArduinoHelper.Property.GetDataPutUrl(ArduinoHelper.Property.PowerKey, on ? "true" : "false");
+            var content2 = ArduinoHelper.SendToArduino(SetPowerValueUrl);
+            Clients.All.debug(String.Format("RedGreenBlue: url={0}, content={1}", SetPowerValueUrl, content2));
+
+            // send status to clients
+            RequestStatus();
+        }
+
+        // SignalR client callback
+        // sets the color of the LEDs
+        public void SetRgb(int red, int green, int blue)
+        {
+            // create new RGB
+            RGB rgb = new RGB();
+            rgb.red = red;
+            rgb.green = green;
+            rgb.blue = blue;
+
+            // send RGB to Arduino
+            SendDXM(rgb);
+
+            // notify clients
+            Clients.All.debug("SetRgb: " + red + "," + green + "," + blue);    
+        }
+
+        // SignalR client callback
+        // records the position of the RGB slider
+        public void SetRgbSlider(float value)
+        {
+            // cache color slider value
+            String sliderValue = ""+value;
+            String SetColorSliderValueUrl = ArduinoHelper.Property.GetDataPutUrl(ArduinoHelper.Property.RgbSliderKey, sliderValue);
+            var content = ArduinoHelper.SendToArduino(SetColorSliderValueUrl);
+            Clients.All.debug(String.Format("SetRgbSlider: url={0}, content={1}", SetColorSliderValueUrl, content));
+
+            // send status to clients
+            RequestStatus();
+        }
+
+        // SignalR client callback
+        // sets the speed and direction of the train
+        public void SetSpeed(float value)
+        {
+            // get current speed slider value
+            String GetSpeedStatusUrl = ArduinoHelper.Property.GetDataGetUrl(ArduinoHelper.Property.SpeedSliderKey);
+            var SpeedJSon = ArduinoHelper.SendToArduino(GetSpeedStatusUrl);
+            Clients.All.debug(String.Format("SetSpeed: url={0}, content={1}", GetSpeedStatusUrl, SpeedJSon));
+            
+            // cache speed slider value
+            String sliderValue = ""+value;
+            String SetSpeedSliderValueUrl = ArduinoHelper.Property.GetDataPutUrl(ArduinoHelper.Property.SpeedSliderKey, sliderValue);
+            var content = ArduinoHelper.SendToArduino(SetSpeedSliderValueUrl);
+            Clients.All.debug(String.Format("SetSpeed: url={0}, content={1}", SetSpeedSliderValueUrl, content));
+
+            // Forward=[0..1.0], Backwards=[-1.0..0], Stop=[0]
+            int speed;
+            String direction;
+            if (value < 0) // backward
+            {
+                speed = (int)(255.0 * (value * -1.0)); // 0-255
+                direction = ArduinoHelper.Property.Backward;
+            }
+            else if (value > 0) // forward
+            {
+                speed = (int)(255.0 * value); // 0-255
+                direction = ArduinoHelper.Property.Forward;
+            }
+            else // stop
+            {
+                speed = 0;
+                direction = ArduinoHelper.Property.Release;
+            }
+            String SetSpeedUrl = ArduinoHelper.Property.GetMotorWriteUrl(speed, direction);
+            content = ArduinoHelper.SendToArduino(SetSpeedUrl);
+            Clients.All.debug(String.Format("SetSpeed: url={0}, content={1}", SetSpeedUrl, content));
+
+            // send status to clients
+            RequestStatus();
+        }
+
+        // SignalR client callback
+        // send status of power, RGB slider, and speed slider
+        public void RequestStatus()
+        {
+            // power
+            String GetPowerStatusUrl = ArduinoHelper.Property.GetDataGetUrl(ArduinoHelper.Property.PowerKey);
+            var PowerStatusJson = ArduinoHelper.SendToArduino(GetPowerStatusUrl);
+            Clients.All.debug(String.Format("Status: url={0}, content={1}", GetPowerStatusUrl, PowerStatusJson));
+            Clients.All.status(PowerStatusJson); 
+
+            // RGB slider
+            String GetColorSliderValueUrl = ArduinoHelper.Property.GetDataGetUrl(ArduinoHelper.Property.RgbSliderKey);
+            var RgbSliderStatusJson = ArduinoHelper.SendToArduino(GetColorSliderValueUrl);
+            Clients.All.debug(String.Format("Status: url={0}, content={1}", GetColorSliderValueUrl, RgbSliderStatusJson));
+            Clients.All.status(RgbSliderStatusJson); 
+
+            // speed slider
+            String GetSpeedSliderValueUrl = ArduinoHelper.Property.GetDataGetUrl(ArduinoHelper.Property.SpeedSliderKey);
+            var SpeedSliderStatusJson = ArduinoHelper.SendToArduino(GetSpeedSliderValueUrl);
+            Clients.All.debug(String.Format("Status: url={0}, content={1}", GetSpeedSliderValueUrl, SpeedSliderStatusJson));
+            Clients.All.status(SpeedSliderStatusJson);
+        }
+
+        private void SendDXM(RGB rgb) {
+            // initialize the engine if needed
+            if (ArtNetEngine == null)
+            {
+                System.Diagnostics.Debug.WriteLine("Staring ArtEngine...");
+                ArtNetStopWatch.Restart();
+                ArtNetEngine = new ArtNet.Engine("ArtNet Engine", "");
+                ArtNetEngine.Start();
+                ArtNetStopWatch.Stop();
+                System.Diagnostics.Debug.WriteLine( String.Format("...took {0}ms", ArtNetStopWatch.ElapsedMilliseconds) );
+            }
+
+            // build DMX data
+            System.Diagnostics.Debug.WriteLine("Building DMXData...");
+            ArtNetStopWatch.Restart();
+            byte[] DMXData = new byte[512];
+            for(int i=0;i<DMXData.Length;i+=3) {
+                DMXData[i] = (byte)rgb.red;
+                if(i+1<DMXData.Length)
+                    DMXData[i + 1] = (byte)rgb.green;
+                if(i+2<DMXData.Length)
+                    DMXData[i + 2] = (byte)rgb.blue;
+            }
+            ArtNetStopWatch.Stop();
+            System.Diagnostics.Debug.WriteLine(String.Format("...took {0}ms", ArtNetStopWatch.ElapsedMilliseconds));
+
+            // send DMX data
+            System.Diagnostics.Debug.WriteLine("Sending ArtEngine data...");
+            ArtNetStopWatch.Restart();
+            ArtNetEngine.SendDMX(0, DMXData, DMXData.Length);
+            ArtNetStopWatch.Stop();
+            System.Diagnostics.Debug.WriteLine(String.Format("...took {0}ms", ArtNetStopWatch.ElapsedMilliseconds));
+        }
+
+        public class RGB
+        {
+            private static byte MIN = (byte)0;
+            private static byte MAX = (byte)255;
+
+            public int red { get; set; }
+            public int green { get; set; }
+            public int blue { get; set; }
+
+            public RGB()
+            {
+                init(MIN, MIN, MIN);
+            }
+
+            public RGB(int red, int green, int blue)
+            {
+                init(red, green, blue);
+            }
+
+            private void init(int red, int green, int blue)
+            {
+                this.red = red;
+                this.green = green;
+                this.blue = blue;
+            }
+        }
+    }
+}
