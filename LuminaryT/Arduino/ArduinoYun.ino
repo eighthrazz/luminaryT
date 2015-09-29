941046430496d1ea3ac1ed4feb1b8729f449f33b
eighthrazz@gmail.com <eighthrazz@gmail.com>
2015-09-28 22:35:07 -0500

    initial commit

---
+++ LuminaryT/Arduino/ArduinoYun.ino
@@ -0,0 +1,222 @@
+﻿#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+
+// listen to the default port 5555, the Yún webserver
+// will forward there all the HTTP requests you send
+YunServer server;
+
+// create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// select which 'port' M1, M2, M3 or M4. In this case, M1
+Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+
+void setup() {
+  // bridge startup
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  digitalWrite(13, HIGH);
+
+  // listen for incoming connection only from localhost
+  // (no one from the external network could connect)
+  server.listenOnLocalhost();
+  server.begin();
+  
+  // create with the default frequency 1.6KHz
+  AFMS.begin();
+  
+  // set the speed to start, from 0 (off) to 255 (max speed)
+  myMotor->setSpeed(150);
+  myMotor->run(BACKWARD);
+}
+
+void loop() {
+  // get clients coming from server
+  YunClient client = server.accept();
+
+  // Is there a new client?
+  if (client) {
+    // process request
+    process(client);
+
+    // close connection and free resources.
+    client.stop();
+  }
+
+  delay(50); // Poll every 50ms
+}
+
+void process(YunClient client) {
+  // read the command
+  String command = client.readStringUntil('/');
+
+  // is "digital" command?
+  if (command == "digital") {
+    digitalCommand(client);
+  }
+
+  // is "analog" command?
+  if (command == "analog") {
+    analogCommand(client);
+  }
+
+  // is "mode" command?
+  if (command == "mode") {
+    modeCommand(client);
+  }
+  
+  // is "motor" command
+  if (command == "motor") {
+     motorCommand(client); 
+  }
+}
+
+void digitalCommand(YunClient client) {
+  int pin, value;
+
+  // read pin number
+  pin = client.parseInt();
+
+  // ff the next character is a '/' it means we have an URL
+  // with a value like: "/digital/13/1"
+  if (client.read() == '/') {
+    value = client.parseInt();
+    digitalWrite(pin, value);
+  }
+  else {
+    value = digitalRead(pin);
+  }
+
+  // Send feedback to client
+  client.print(F("Pin D"));
+  client.print(pin);
+  client.print(F(" set to "));
+  client.println(value);
+
+  // Update datastore key with the current pin value
+  String key = "D";
+  key += pin;
+  Bridge.put(key, String(value));
+}
+
+void analogCommand(YunClient client) {
+  int pin, value;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is a '/' it means we have an URL
+  // with a value like: "/analog/5/120"
+  if (client.read() == '/') {
+    // Read value and execute command
+    value = client.parseInt();
+    analogWrite(pin, value);
+
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" set to analog "));
+    client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "A";
+    key += pin;
+    Bridge.put(key, String(value));
+  }
+  else {
+    // Read analog pin
+    value = analogRead(pin);
+
+    // Send feedback to client
+    client.print(F("Pin A"));
+    client.print(pin);
+    client.print(F(" reads analog "));
+    client.println(value);
+
+    // Update datastore key with the current pin value
+    String key = "A";
+    key += pin;
+    Bridge.put(key, String(value));
+  }
+}
+
+void modeCommand(YunClient client) {
+  int pin;
+
+  // Read pin number
+  pin = client.parseInt();
+
+  // If the next character is not a '/' we have a malformed URL
+  if (client.read() != '/') {
+    client.println(F("error"));
+    return;
+  }
+
+  String mode = client.readStringUntil('\r');
+
+  if (mode == "input") {
+    pinMode(pin, INPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as INPUT!"));
+    return;
+  }
+
+  if (mode == "output") {
+    pinMode(pin, OUTPUT);
+    // Send feedback to client
+    client.print(F("Pin D"));
+    client.print(pin);
+    client.print(F(" configured as OUTPUT!"));
+    return;
+  }
+
+  client.print(F("error: invalid mode "));
+  client.print(mode);
+}
+
+// ex: /motor/(speed:0-255)/(direction:forward,backward,release))
+void motorCommand(YunClient client) {
+  int speed;
+
+  // read speed
+  speed = client.parseInt();
+
+  // if the next character is not a '/' we have a malformed URL
+  if (client.read() != '/') {
+    client.println(F("error"));
+    return;
+  }
+
+  // set speed
+  myMotor->setSpeed(speed);
+
+  // direction
+  String direction = client.readStringUntil('\r');
+  if (direction == "forward") {  
+    myMotor->run(FORWARD);      
+  }
+  else if (direction == "backward") {
+    myMotor->run(BACKWARD);  
+  }
+  else if (direction == "release") {
+    myMotor->run(RELEASE);    
+  }
+  else {
+    client.print(F("error: invalid direction "));
+    client.print(direction);
+    return;
+  }
+
+  // send feedback to client
+  client.print(F("Motor "));
+  client.print(direction);
+  client.print(F(" at speed "));
+  client.println(speed);
+}
---
+++ LuminaryT/ArduinoHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Luminary
+{
+    public class ArduinoHelper
+    {
+        public static String SendToArduino(String url)
+        {
+            System.Diagnostics.Debug.WriteLine( String.Format("SendToArduino: url={0}", url) );
+            System.Net.WebClient ArduinoWebClient = null;
+            try
+            {
+                ArduinoWebClient = new System.Net.WebClient();
+                var content = ArduinoWebClient.DownloadString(url);
+                return content;
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(String.Format("SendToArduino: error={0}", e));
+                return "error"+e;
+            }
+            finally
+            {
+                if (ArduinoWebClient != null)
+                    ArduinoWebClient.Dispose();
+            }
+        }
+
+        public class Property
+        {
+            public static readonly int PowerPin = 12;
+            public static readonly int High = 1;
+            public static readonly int Low = 0;
+
+            public static readonly String PowerKey = "power";
+            public static readonly String RgbSliderKey = "rgbSlider";
+            public static readonly String SpeedSliderKey = "speedSlider";
+
+            public static readonly String Forward = "forward";
+            public static readonly String Backward = "backward";
+            public static readonly String Release = "release";
+
+            public static readonly string IP = "192.168.1.165";
+            public static readonly string BaseUrl = "http://" + IP;
+            public static readonly string ArduinoUrl = BaseUrl + "/arduino";
+            public static readonly string DataUrl = BaseUrl + "/data";
+
+            public static string GetDigitalReadUrl(int pin)
+            {
+                return ArduinoUrl + "/digital/" + pin;
+            }
+
+            public static string GetDigitalWriteUrl(int pin, int value)
+            {
+                return ArduinoUrl + "/digital/" + pin + "/" + value;
+            }
+
+            public static string GetAnalogReadUrl(int pin)
+            {
+                return ArduinoUrl + "/analog/" + pin;
+            }
+
+            public static string GetAnalogWriteUrl(int pin, int value)
+            {
+                return ArduinoUrl + "/analog/" + pin + "/" + value;
+            }
+
+            public static string GetModeInputUrl(int pin)
+            {
+                return ArduinoUrl + "/mode/" + pin + "/input";
+            }
+
+            public static string GetModeOutputUrl(int pin)
+            {
+                return ArduinoUrl + "/mode/" + pin + "/output";
+            }
+
+            public static string GetDataPutUrl(String key, String value)
+            {
+                return DataUrl + "/put/" + key + "/" + value;
+            }
+
+            public static string GetDataGetUrl(String key)
+            {
+                return DataUrl + "/get/" + key;
+            }
+
+            public static string GetDigitalKey(int pin)
+            {
+                return "D" + pin;
+            }
+
+            public static string getAnalogKey(int pin)
+            {
+                return "A" + pin;
+            }
+
+            public static string GetMotorWriteUrl(int speed, String direction)
+            {
+                return ArduinoUrl + "/motor/" + speed + "/" + direction;
+            }
+        }
+    }
+}
